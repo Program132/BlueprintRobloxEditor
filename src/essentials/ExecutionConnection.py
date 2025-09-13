a32ae25e3297533b03724bc14090681752a9ded2
Program <67320703+Program132@users.noreply.github.com>
2025-09-14 01:08:23 +0200

    Fix ExecutionConnection.py, updated logic

--- src/essentials/ExecutionConnection.py
+++ src/essentials/ExecutionConnection.py
@@ -3,3 +3,7 @@ class ExecutionConnection:
     def __init__(self, from_node, to_node):
         self.from_node = from_node
         self.to_node = to_node
+
+        if not hasattr(from_node, "exec_outputs"):
+            from_node.exec_outputs = []
+        from_node.exec_outputs.append(to_node)
