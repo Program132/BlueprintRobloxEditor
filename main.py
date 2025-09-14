af538ec2f2f56fafefb0b788180f4814dd03de80
Program <67320703+Program132@users.noreply.github.com>
2025-09-14 15:08:51 +0200

    New example

--- main.py
+++ main.py
@@ -1,46 +1,42 @@
-from src.Engine import Engine
-from src.essentials.ExecutionConnection import ExecutionConnection
-from src.nodes.BooleanValue import BooleanValueNode
-from src.nodes.DoubleValue import DoubleValueNode
-from src.nodes.IntValue import IntValueNode
+# main.py
+from src.nodes.Variable import VariableNode
+from src.nodes.GetVariable import GetVariableNode
+from src.nodes.SetVariable import SetVariableNode
 from src.nodes.Print import PrintNode
-from src.nodes.StringValue import StringValueNode
-from src.nodes.EventStart import EventStartNode
 from src.essentials.DataConnection import DataConnection
+from src.Engine import Engine
+from src.nodes.EventStart import EventStartNode
+from src.essentials.ExecutionConnection import ExecutionConnection
 
 engine = Engine()
 
-eventStart = EventStartNode()
-print1 = PrintNode()
-print2 = PrintNode()
-print3 = PrintNode()
-print4 = PrintNode()
+# Declaration: "score"
+myVar = VariableNode("score")
+myVar.updateValue(10)
 
-stringVal = StringValueNode()
-stringVal.updateValueOutput("value", "Hello World 2!")
-intVal = IntValueNode()
-intVal.updateValueOutput("value", 18)
-doubleVal = DoubleValueNode()
-doubleVal.updateValueOutput("value", 3.14)
-boolVal = BooleanValueNode()
-boolVal.updateValueOutput("value", True)
+getNode = GetVariableNode()
+printNode = PrintNode()
+setNode = SetVariableNode()
+setNode.updateValueInput("value", 42)
+get2Node = GetVariableNode()
+print2Node = PrintNode()
 
 
 
-conn = DataConnection(stringVal, "value", print1, "value")
-conn.propagate()
-conn = DataConnection(intVal, "value", print2, "value")
-conn.propagate()
-conn = DataConnection(doubleVal, "value", print3, "value")
-conn.propagate()
-conn = DataConnection(boolVal, "value", print4, "value")
-conn.propagate()
+engine.addDataConnection(DataConnection(myVar, "ref", getNode, "ref"))
+engine.addDataConnection(DataConnection(getNode, "value", printNode, "value"))
+engine.addDataConnection(DataConnection(myVar, "ref", setNode, "ref"))
+engine.addDataConnection(DataConnection(myVar, "ref", get2Node, "ref"))
+engine.addDataConnection(DataConnection(get2Node, "value", print2Node, "value"))
 
 
-engine.addExecutionConnection(ExecutionConnection(eventStart, print1))
-engine.addExecutionConnection(ExecutionConnection(print1, print2))
-engine.addExecutionConnection(ExecutionConnection(print2, print3))
-engine.addExecutionConnection(ExecutionConnection(print3, print4))
+start = EventStartNode()
+engine.addExecutionConnection(ExecutionConnection(start, myVar))
+engine.addExecutionConnection(ExecutionConnection(myVar, getNode))
+engine.addExecutionConnection(ExecutionConnection(getNode, printNode))
+engine.addExecutionConnection(ExecutionConnection(start, setNode))
+engine.addExecutionConnection(ExecutionConnection(setNode, get2Node))
+engine.addExecutionConnection(ExecutionConnection(get2Node, print2Node))
 
-#engine.generateFile("main.lua")
+# Génération
 print(engine)
