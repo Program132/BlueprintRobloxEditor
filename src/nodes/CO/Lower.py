eb405cf3e3ab1c5b582108100362960490ea093d
Program <67320703+Program132@users.noreply.github.com>
2025-09-16 19:09:08 +0200

    Operators

---
+++ src/nodes/CO/Lower.py
@@ -0,0 +1,27 @@
+# src/nodes/CO/Lower.py
+from src.essentials.Node import Node
+from src.essentials.NodeType import NodeType
+
+class LowerNode(Node):
+    def __init__(self):
+        super().__init__(NodeType.METHOD, "Lower (<)")
+        self.addInput("value1", None)
+        self.addInput("value2", None)
+        self.addOutput("result", None)
+
+    def getValue(self):
+        n1 = self.getValueInput("value1")
+        n2 = self.getValueInput("value2")
+        if isinstance(n1, Node):
+            n1 = n1.getValue()
+        if isinstance(n2, Node):
+            n2 = n2.getValue()
+        if n1 is None: n1 = 0
+        if n2 is None: n2 = 0
+
+        expr = f"{n1} < {n2}"
+        self.updateValueOutput("result", expr)
+        return expr
+
+    def toLuau(self):
+        return None
