814816588c5f378f0eefee4d650c05ba3ac410e7
Program <67320703+Program132@users.noreply.github.com>
2025-11-10 19:50:45 +0100

    Example 1.3 (main.py for tests)

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 from src.Engine.Engine import Engine
-from src.Nodes.Models.Print import Print
-from src.Nodes.Models.math.Addition import Addition
 from src.Nodes.Transition import Transition
 from src.Nodes.TransitionType import TransitionType
+from src.Nodes.NodeType import NodeType
 from src.Nodes.Events.Start import Start
+from src.Nodes.Models.Variables.Variable import Variable
+from src.Nodes.Models.Print import Print
+from src.Nodes.Models.Variables.GET import GET
+from src.Nodes.Models.Variables.SET import SET
+
+var_message = Variable("myMessage", "Hello World")
 
 START = Start()
+get_node_1 = GET()
+get_node_1.setInputValue("name", "myMessage")
 
-add_node = Addition()
-add_2_node = Addition()
-add_3_node = Addition()
-print_node = Print()
+print_node_1 = Print()
+set_node = SET()
+set_node.setInputValue("name", "myMessage")
+set_node.setInputValue("value", "Hello World 2.0")
 
-add_node.setInputValue("a", 5)
-add_node.setInputValue("b", 7)
+get_node_2 = GET()
+get_node_2.setInputValue("name", "myMessage")
 
-add_2_node.setInputValue("a", 20)
+print_node_2 = Print()
+print_node_2.type = NodeType.METHOD
 
-add_3_node.setInputValue("a", 100)
+exec_1 = Transition(START, print_node_1, TransitionType.EXEC)
+exec_2 = Transition(print_node_1, set_node, TransitionType.EXEC)
+exec_3 = Transition(set_node, print_node_2, TransitionType.EXEC)
 
-exec_link = Transition(START, print_node, TransitionType.EXEC) # START -> print
-date_link_add = Transition(add_node, add_2_node, TransitionType.DATA, add_2_node.getInput("b"), add_node.getOutput("result")) # add result -> add_2 b
-date_link2_add = Transition(add_2_node, add_3_node, TransitionType.DATA, add_3_node.getInput("b"), add_2_node.getOutput("result")) # add_2 result -> add_3 b
-date_link_print = Transition(add_3_node, print_node, TransitionType.DATA, print_node.getInput("value"), add_3_node.getOutput("result")) # add_3 result -> print
+
+data_1 = Transition(get_node_1, print_node_1, TransitionType.DATA,
+                    print_node_1.getInput("value"), get_node_1.getOutput("value"))
+
+data_2 = Transition(get_node_2, print_node_2, TransitionType.DATA,
+                    print_node_2.getInput("value"), get_node_2.getOutput("value"))
 
 engine = Engine()
+
+engine.addVariable(var_message)
+
 engine.addNode(START)
-engine.addNode(add_node)
-engine.addNode(add_2_node)
-engine.addNode(add_3_node)
-engine.addNode(print_node)
-engine.addTransition(exec_link)
-engine.addTransition(date_link_add)
-engine.addTransition(date_link2_add)
-engine.addTransition(date_link_print)
+engine.addNode(get_node_1)
+engine.addNode(print_node_1)
+engine.addNode(set_node)
+engine.addNode(get_node_2)
+engine.addNode(print_node_2)
+
+engine.addTransition(exec_1)
+engine.addTransition(exec_2)
+engine.addTransition(exec_3)
+engine.addTransition(data_1)
+engine.addTransition(data_2)
 
 luau_code = engine.generateLuau()
 print(luau_code)
