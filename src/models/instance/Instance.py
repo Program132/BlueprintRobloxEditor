89ab693538ecf5e2626eaf15ff412339f00455e5
program <clement.gauthier132@orange.fr>
2025-11-21 10:55:28 +0100

    Instance New node to create instances from Roblox (part, model, intvalue, etc.)

---
+++ src/models/instance/Instance.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from src.Node import Node
+
+
+class Instance(Node):
+    def __init__(self) -> None:
+        super().__init__("nodes/instance/instance.json")
+
+    def toLuau(self) -> Optional[str]:
+        classname = self.getInputValue("ClassName")
+        parent = self.getInputValue("parent")
+
+        classname_formatted = self._formatValueForLuau(classname)
+        
+        if parent == "" or len(parent) == 0:
+            r = f'Instance.new({classname_formatted})'
+        else:
+            parent_formatted = self._formatValueForLuau(parent)
+            r = f'Instance.new({classname_formatted}, {parent_formatted})'
+        
+        self.setOutputValue("instance", r)
+        return r
