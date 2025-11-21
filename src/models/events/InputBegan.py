a628d32fc58314feb853bde0d344c70892941384
Program <67320703+Program132@users.noreply.github.com>
2025-11-21 19:15:02 +0100

    Fix about detection of type + new events: inputended & inputbegan

--- src/models/events/InputBegan.py
+++ src/models/events/InputBegan.py
@@ -2,20 +2,21 @@ from typing import Optional
 from src.Node import Node
 from src.Variable import Variable
 
-class PlayerAdded(Node):
+
+class InputBegan(Node):
     def __init__(self) -> None:
-        super().__init__("nodes/events/playeradded.json")
+        super().__init__("nodes/events/inputbegan.json")
         self.events_count = 1
 
     def toLuau(self) -> Optional[str]:
         output_names = [o.name for o in self.outputs]
         args = ", ".join(output_names)
         
-        
+       
         for output in self.outputs:
             self.setOutputValue(output.name, output.name)
             if not any(v.name == output.name for v in self.engine.variables):
                 self.engine.variables.append(Variable(output.name, output.name))
         
-        r = f'game:GetService("Players").PlayerAdded:Connect(function({args})'
+        r = f'game:GetService("UserInputService").InputBegan:Connect(function({args})'
         return r
--- src/models/events/InputEnded.py
+++ src/models/events/InputEnded.py
@@ -1,21 +1,21 @@
 from typing import Optional
 from src.Node import Node
-from src.Variable import Variable
 
-class PlayerAdded(Node):
+
+class InputEnded(Node):
     def __init__(self) -> None:
-        super().__init__("nodes/events/playeradded.json")
+        super().__init__("nodes/events/inputended.json")
         self.events_count = 1
 
     def toLuau(self) -> Optional[str]:
         output_names = [o.name for o in self.outputs]
         args = ", ".join(output_names)
         
-        
+        from src.Variable import Variable
         for output in self.outputs:
             self.setOutputValue(output.name, output.name)
             if not any(v.name == output.name for v in self.engine.variables):
                 self.engine.variables.append(Variable(output.name, output.name))
         
-        r = f'game:GetService("Players").PlayerAdded:Connect(function({args})'
+        r = f'game:GetService("UserInputService").InputEnded:Connect(function({args})'
         return r
