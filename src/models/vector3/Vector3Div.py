901cff924394294b1d89490d5c3035ffc65e9c5b
Program <67320703+Program132@users.noreply.github.com>
2025-11-22 16:08:10 +0100

    New nodes about Vector3

---
+++ src/models/vector3/Vector3Div.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from src.Node import Node
+
+class Vector3Div(Node):
+    def __init__(self) -> None:
+        super().__init__("nodes/vector3/vector3div.json")
+
+    def toLuau(self) -> Optional[str]:
+        vec1 = self.getInputValue("vec1")
+        vec2 = self.getInputValue("vec2")
+        r = f"{vec1} / {vec2}"
+        
+        self.setOutputValue("Vector3", r)
