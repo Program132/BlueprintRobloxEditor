61cbc73142b76f9d0f7aea7b4e5f14bdcad216e1
Program <67320703+Program132@users.noreply.github.com>
2025-11-23 15:37:17 +0100

    Touched event added

---
+++ src/models/events/Touched.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from src.Node import Node
+from src.Variable import Variable
+
+
+class Touched(Node):
+    def __init__(self) -> None:
+        super().__init__("nodes/events/touched.json")
+        self.events_count = 1
+
+    def toLuau(self) -> Optional[str]:
+        instance = self.getInputValue("instance")
+        output_names = [o.name for o in self.outputs]
+        args = ", ".join(output_names)
+        
+        for output in self.outputs:
+            self.setOutputValue(output.name, output.name)
+            if not any(v.name == output.name for v in self.engine.variables):
+                self.engine.variables.append(Variable(output.name, output.name))
+        
+        r = f'{instance}.Touched:Connect(function({args})'
+        return r
